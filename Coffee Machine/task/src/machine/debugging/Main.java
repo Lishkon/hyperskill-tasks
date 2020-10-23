d2ff01807a4d088372448ceb116ac33c722eb62f
Alex Lychak <lishkon@gmail.com>
2020-10-23 17:47:55 +0300

    The sum of array elements solved

--- Coffee Machine/task/src/machine/debugging/Main.java
+++ Coffee Machine/task/src/machine/debugging/Main.java
@@ -4,18 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int i;
-        do {
-            System.out.println("test");
-            i = scanner.nextInt();
-            if (i == 1) {
-                System.out.println("first");
-            } else if (i == 2) {
-                System.out.println("second");
-            } else {
-                break;
-            }
-        } while (true);
+        int [] nums = {1,2,3};
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(nums[i]);
+        }
     }
 }
