8e11d5b57c9d4c4d8107fb886f80c67d849c035a
Alex Lychak <lishkon@gmail.com>
2020-10-24 22:31:58 +0300

    solved the Circle task

---
+++ Problems/Circle/src/Main.java
@@ -0,0 +1,16 @@
+import static java.lang.Math.round;
+
+class Circle {
+
+    double radius;
+
+    // write methods here
+    public double getLength() {
+        return 2 * Math.PI * radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+}
