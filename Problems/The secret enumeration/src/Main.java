d64e0e2004de647f2fcf0ec945eed2b5e4dc4833
Alex Lychak <lishkon@gmail.com>
2020-10-25 23:20:47 +0200

    Secret enumeration is solved

---
+++ Problems/The secret enumeration/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Secret[] enumArrayed = Secret.values();
+        int count = 0;
+        for (Secret secret : enumArrayed) {
+            if (secret.name().startsWith("STAR")) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+/* At least two constants start with STAR
+enum Secret {
+    STAR, CRASH, START, TEST, STARRIOR
+}*/
+
