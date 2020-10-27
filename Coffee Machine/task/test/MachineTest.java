786f011f7274be47b18974b0bbc1e57cd83c9ed6
Alex Lychak <lishkon@gmail.com>
2020-10-27 15:15:09 +0200

    CoffeeMachine & Machine tests

--- Coffee Machine/task/test/MachineTest.java
+++ Coffee Machine/task/test/MachineTest.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import org.junit.Test;
 
 public class MachineTest {
+    @Test
+    public void buyTest() {
+        //TODO write the tests for buy() method
+    }
+
+    @Test
+    public void fillTest() {
+        //todo write the tests for fill() method
+    }
+
+    @Test
+    public void startTest() {
+        //todo write the tests for start() method
+    }
+
+    @Test
+    public void exitTest() {
+        //todo write the tests for exit() method
+    }
+
+    @Test
+    public void checkResourcesTest() {
+        //TODO write the tests for the checkResources() method
+    }
 }
